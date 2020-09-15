b8f6c7640105480b50e7f6f3269e5042af7f8d5a
jason-nordheim <jason.nordheim@gmail.com>
2020-09-15 17:17:33 -0600

    created Tile class

---
+++ src/Tile.ts
@@ -0,0 +1,11 @@
+export class Tile {
+  x: number 
+  y: number 
+  value: any 
+  
+  constructor(x: number, y:number, value:any) {
+    this.x = x
+    this.y = y 
+    this.value = value 
+  }
+}
