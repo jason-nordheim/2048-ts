6f0618f91cfff29c20ab358d67d47cd8dbc9af17
jason-nordheim <jason.nordheim@gmail.com>
2020-09-15 21:48:57 -0600

    removed

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,2 @@
-import { Game } from './resources/Game'
+import { Game } from './lib/models/Game'
 
-/**
- * Create the Game 
- */
-const game = new Game(4,4) 
-
-/**
- * Start the game 
- */
-game.start()
-
-/**
- * While the game isn't finished, 
- * place a piece on the board, 
- * await a player making a move, 
- * then slide the tiles on the board 
- * 
- */
-while(!game.isFinished()) {
-
-}
